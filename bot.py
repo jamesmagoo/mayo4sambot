5bb90883bc23efbded74377dadd4655909061e80
James McGauran <jamesmcgauran11@gmail.com>
2021-02-04 20:22:57 +0000

    Write tweet on interval added

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import time
 from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
 
@@ -15,37 +16,34 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Get Today's date object
-# Subtract it from 23/09/1951
-# Calculate years, days, minutes, seconds
-# Tweet numbers as part of template string using api.update_status()
+# Initiate date obbject of last win
+lastWin = datetime(1951, 9, 23, 16, 45)
 
+# Initiate today's date
+today = datetime.today()
 
-def calculateSince():
-    lastWin = datetime(1951, 9, 23, 16, 45)
-    today = datetime.today()
+# Calculate the difference between the two dates
+diff = relativedelta(today, lastWin,)
+years = str(diff.years)
+months = str(diff.months)
+days = str(diff.days)
+hours = str(diff.hours)
+minutes = str(diff.minutes)
 
-    timedelta = today-lastWin
+# Compose string to tweet
+line = str('It has been ' + years + ' years, ' + months + ' months, ' + days +
+           ' days, ' + hours + ' hours, and ' + minutes + ' minutes, since Mayo won Sam.')
 
-    diff = relativedelta(today, lastWin,)
-    years = str(diff.years)
-    months = str(diff.months)
-    days = str(diff.days)
-    hours = str(diff.hours)
-    minutes = str(diff.minutes)
+# Set period to tweet (At random time between 5-10 days)
+interval = 15  # 15s for testing
 
-    print(timedelta)
-    print(diff)
-    print('It has been ' + years + ' years, ' + months + ' months, ' + days +
-          ' days, ' + hours + ' hours, and ' + minutes + ' minutes, since Mayo won Sam.')
+while True:
+    print(line)
+    time.sleep(interval)
 
-
-calculateSince()
-
-# api.update_status('hello world')
+# api.update_status(line)
 
 # Reply to tweet 'How Long?' with the time elapsed calculated previously
-
 # Get mentions object mentions = api.mentions_timeline()
 # Check if mentions.text contains string 'How Long?'
 # reply with time
