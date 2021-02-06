ab03300651dd215b57933e4ef606a90499a10cf9
James McGauran <jamesmcgauran11@gmail.com>
2021-02-06 18:59:02 +0000

    Tidy to one script , add outline

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@
 import tweepy
 import os
 import time
-from calculateTime import *
-
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
 from random import randrange
 
 from dotenv import load_dotenv
@@ -19,6 +19,30 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+# Calculate the time since Mayo last won the All-Ireland
+
+def calculateTime():
+    # Initiate date object of last win
+    lastWin = datetime(1951, 9, 23, 16, 45)
+
+    # Initiate today's date
+    today = datetime.today()
+
+    # Calculate the difference between the two dates
+    diff = relativedelta(today, lastWin,)
+    years = str(diff.years)
+    months = str(diff.months)
+    days = str(diff.days)
+    hours = str(diff.hours)
+    minutes = str(diff.minutes)
+
+    # Compose string to tweet
+    line = str('It has been ' + years + ' years, ' + months + ' months, ' + days +
+               ' days, ' + hours + ' hours, and ' + minutes + ' minutes, since Mayo won Sam.')
+
+    return line
+
+# Set interval between Tweets
 
 def setInterval():
     # Set period to tweet (At random time between 5-10 days)
@@ -27,8 +51,60 @@ def setInterval():
     # interval = 1 * rand  # for testing
     return interval
 
+FILE_NAME = 'last_seen_id.txt'
 
-# Tweet
+def retrieveLastSeen(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def storeLastSeen(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+def replyBot():
+    # Reply to mentions with the following dict items only
+    question = ['How long?', 'long', 'how many']
+    # Get the last seen tweet id
+    last_seen_id = retrieveLastSeen()
+    # Get mentions object
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode="extended")
+    # Check if mentions.text contains string 'How Long?'
+    for mention in reversed(mentions):
+        # if no mentions abort
+        if not mention:
+            return
+        # check if the mention contains a keyword
+        else : mention.text ###################
+        # get user name
+        user = api.get_user.screen_name
+        # get the timeSince
+        answer = calculateTime()
+        # make msg
+        msg = user + answer
+        # reply 
+        api.update_status(msg, mention.id)
+        # save as last_seen
+        last_seen_id = mention.id
+        storeLastSeen(last_seen_id, FILE_NAME)
+        # else save as last_seen and abort
+        else :
+            last_seen_id = mention.id
+            storeLastSeen(last_seen_id, FILE_NAME)
+            return
+
+def sendTweet():
+    # send tweet periodically
+    line = calculateTime()
+    # print(line) #testing
+    api.update_status(line)
+
+
 while True:
     line = calculateTime()
     # print(line) #testing
